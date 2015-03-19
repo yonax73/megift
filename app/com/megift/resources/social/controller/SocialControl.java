d09f96d20f6d2c0192a89acb3a88dc83f6437b90
Yonatan Alexis Quintero Rodriguez <yonax73@gmail.com>
2015-03-18 23:11:54 -0500

    try catch socialcontrol

--- app/com/megift/resources/social/controller/SocialControl.java
+++ app/com/megift/resources/social/controller/SocialControl.java
@@ -4,6 +4,7 @@ import static com.megift.resources.utils.Constants.SUCCESS_RESPONSE;
 
 import java.util.Map;
 
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -14,31 +15,36 @@ import com.megift.sec.login.entity.Login;
 public class SocialControl extends Controller {
 
 	public static Result inviteFriends() {
-		response().setHeader("Access-Control-Allow-Origin", "*");
-		final Map<String, String[]> data = request().body().asFormUrlEncoded();
-		String result = "Error al recibir la petición";
-		if (data != null) {
-			String emailFriend1 = data.get("email-friend-1")[0];
-			String emailFriend2 = data.get("email-friend-2")[0];
-			String namePartner = data.get("name-partner")[0];
-			String emailPartner = data.get("email-partner")[0];
-			String emails[] = new String[2];
-			if (emailFriend1 != null)
-				emails[0] = emailFriend1;
-			if (emailFriend2 != null)
-				emails[1] = emailFriend2;
-			if (emails.length > 0) {
-				Partner partner = new Partner(namePartner);
-				partner.setLogin(new Login(emailPartner, null));
-				if (SocialLogic.inviteFriendsByEmail(partner, emails)) {
-					result = SUCCESS_RESPONSE;
+		try {
+			response().setHeader("Access-Control-Allow-Origin", "*");
+			final Map<String, String[]> data = request().body().asFormUrlEncoded();
+			String result = "Error al recibir la petición";
+			if (data != null) {
+				String emailFriend1 = data.get("email-friend-1")[0];
+				String emailFriend2 = data.get("email-friend-2")[0];
+				String namePartner = data.get("name-partner")[0];
+				String emailPartner = data.get("email-partner")[0];
+				String emails[] = new String[2];
+				if (emailFriend1 != null)
+					emails[0] = emailFriend1;
+				if (emailFriend2 != null)
+					emails[1] = emailFriend2;
+				if (emails.length > 0) {
+					Partner partner = new Partner(namePartner);
+					partner.setLogin(new Login(emailPartner, null));
+					if (SocialLogic.inviteFriendsByEmail(partner, emails)) {
+						result = SUCCESS_RESPONSE;
+					} else {
+						result = "Error al enviar invitaciones por correo";
+					}
 				} else {
-					result = "Error al enviar invitaciones por correo";
+					result = "No hay correos para invitar a tus amigos";
 				}
-			} else {
-				result = "No hay correos para invitar a tus amigos";
 			}
+			return ok(result);
+		} catch (Exception e) {
+			Logger.error("Ha ocurrido un error intentado invitar amigos \n" + e.getMessage(), e);
+			return badRequest("Ha ocurrido un error intentado invitar amigos ( " + e.getMessage() + " )");
 		}
-		return ok(result);
 	}
 }
