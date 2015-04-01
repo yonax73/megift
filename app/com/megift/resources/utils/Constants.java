103c2344b8f2d94609941acab76dcbf56a6d993b
Yonatan Alexis Quintero Rodriguez <yonax73@gmail.com>
2015-04-01 16:56:16 -0500

    changes elepase time by hours

--- app/com/megift/resources/utils/Constants.java
+++ app/com/megift/resources/utils/Constants.java
@@ -1,7 +1,6 @@
 package com.megift.resources.utils;
 
 import java.text.DecimalFormat;
-import java.util.Date;
 
 public class Constants {
 	public final static String DATA_TEMP_PATH = "public/tmp/data/";
@@ -72,35 +71,4 @@ public class Constants {
 		}
 	}
 
-	/*
-	 * elapsed time in days, hours, minutes and seconds format
-	 */
-	public String getElapsaTime(Date startDate, Date endDate) {
-
-		// milliseconds
-		long different = endDate.getTime() - startDate.getTime();
-
-		System.out.println("startDate : " + startDate);
-		System.out.println("endDate : " + endDate);
-		System.out.println("different : " + different);
-
-		long secondsInMilli = 1000;
-		long minutesInMilli = secondsInMilli * 60;
-		long hoursInMilli = minutesInMilli * 60;
-		long daysInMilli = hoursInMilli * 24;
-
-		long elapsedDays = different / daysInMilli;
-		different = different % daysInMilli;
-
-		long elapsedHours = different / hoursInMilli;
-		different = different % hoursInMilli;
-
-		long elapsedMinutes = different / minutesInMilli;
-		different = different % minutesInMilli;
-
-		long elapsedSeconds = different / secondsInMilli;
-
-		return String.format("%d days, %d hours, %d minutes, %d seconds%n", elapsedDays, elapsedHours, elapsedMinutes, elapsedSeconds);
-
-	}
 }
--- app/com/megift/resources/utils/Utils.java
+++ app/com/megift/resources/utils/Utils.java
@@ -1,5 +1,10 @@
 package com.megift.resources.utils;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 import com.megift.bsp.action.entity.Action;
@@ -23,5 +28,79 @@ public class Utils {
 			buffer.append(",");
 		}
 		return buffer.toString();
+	}
+
+	/*
+	 * elapsed time in days, hours, minutes and seconds format
+	 */
+	public static String getElapsaTime(Date startDate, Date endDate) {
+
+		// milliseconds
+		long different = endDate.getTime() - startDate.getTime();
+
+		long secondsInMilli = 1000;
+		long minutesInMilli = secondsInMilli * 60;
+		long hoursInMilli = minutesInMilli * 60;
+		long daysInMilli = hoursInMilli * 24;
+
+		long elapsedDays = different / daysInMilli;
+		different = different % daysInMilli;
+
+		long elapsedHours = different / hoursInMilli;
+		different = different % hoursInMilli;
+
+		long elapsedMinutes = different / minutesInMilli;
+		different = different % minutesInMilli;
+
+		long elapsedSeconds = different / secondsInMilli;
+
+		return String.format("%d days, %d hours, %d minutes, %d seconds%n", elapsedDays, elapsedHours, elapsedMinutes, elapsedSeconds);
+
+	}
+
+	/*
+	 * elapsed time hours, minutes and seconds format
+	 */
+	public static String getElapsaTime(LocalDateTime startLocalDateTime, LocalDateTime endLocalDateTime) {
+
+		Date startDate = asDate(startLocalDateTime);
+		Date endDate = asDate(endLocalDateTime);
+		// milliseconds
+		long different = endDate.getTime() - startDate.getTime();
+
+		long secondsInMilli = 1000;
+		long minutesInMilli = secondsInMilli * 60;
+		long hoursInMilli = minutesInMilli * 60;
+		long daysInMilli = hoursInMilli * 24;
+
+		long elapsedDays = different / daysInMilli;
+		different = different % daysInMilli;
+
+		long elapsedHours = different / hoursInMilli;
+		different = different % hoursInMilli;
+
+		long elapsedMinutes = different / minutesInMilli;
+		different = different % minutesInMilli;
+
+		long elapsedSeconds = different / secondsInMilli;
+
+		return String.format("%d:%d:%d", elapsedHours * elapsedDays, elapsedMinutes, elapsedSeconds);
+
+	}
+
+	public static Date asDate(LocalDate localDate) {
+		return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	public static Date asDate(LocalDateTime localDateTime) {
+		return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+	}
+
+	public static LocalDate asLocalDate(Date date) {
+		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+
+	public static LocalDateTime asLocalDateTime(Date date) {
+		return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
 	}
 }
